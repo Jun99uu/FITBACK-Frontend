1fbc0df9888a64d5ddc6a077811529482709a25d
Jun99uu <igun0423@naver.com>
2022-11-25 01:46:35 +0900

    add : 기본 레이아웃 설정

--- fitback-frontend/pages/_app.tsx
+++ fitback-frontend/pages/_app.tsx
@@ -1,11 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </RecoilRoot>
   );
 }
